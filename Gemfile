7eada5508977155e34e27ea5ee466882da72a95a
Eric Mueller <nevinera@gmail.com>
2011-10-03 20:38:56 -0400

    initial project with gems

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://rubygems.org'
+
+gem 'foreman'
+gem 'rails', '3.1.0'
+gem 'thin'
+
+gem 'mysql2'
+
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+gem 'haml'
+gem 'haml-rails'
+
+gem 'devise'
+gem 'cancan'
+
+gem 'heroku'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vacation::Application.config.secret_token = '4084bd98d4397c82341b4b385e738af2cf335877e6aa440a6b7489a6475f09f376ed2e8f14e183a5fe372826d2c1cb94bddd92097d916542724dc729e974bbb6'
